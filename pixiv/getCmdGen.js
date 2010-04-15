db29ea2f7986db86110ebbfa3882d2f8e4f39ce2
xaicron <xaicron@gmail.com>
2010-04-15 23:33:54 +0900

    * ダブルクォートで囲うようにした * キャンセルが効くようにした

--- pixiv/getCmdGen.js
+++ pixiv/getCmdGen.js
@@ -1,6 +1,7 @@
 javascript:(function(d,i,p){
-	d=document.getElementById('profile').childNodes[1].textContent.replace(/^[\s\n]|[\s\n]$/mg,'');
-	i=location.href.match(/id=(\d+)/)[1];
-	p=prompt('page?', 1);
-	prompt('command', 'pixiv_get.pl -s -i ' + i + ' -d ' + d + ' -p ' + p);
+    d=document.getElementById('profile').childNodes[1].textContent.replace(/^[\s\n]|[\s\n]$/mg,'');
+    i=location.href.match(/id=(\d+)/)[1];
+    if(p=prompt('page?', 1)) {
+        prompt('command', 'pixiv_get.pl -s -i ' + i + ' -d "' + d + '" -p ' + p);
+    }
 })();
