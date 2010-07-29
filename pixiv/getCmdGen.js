0a48353e60c2fa1684737ce491da8053fd221c39
xaicron <xaicron@gmail.com>
2010-07-29 12:44:07 +0900

    for the new layout

--- pixiv/getCmdGen.js
+++ pixiv/getCmdGen.js
@@ -1,5 +1,5 @@
 javascript:(function(d,i,p){
-    d=document.getElementById('profile').childNodes[1].textContent.replace(/^[\s\n]|[\s\n]$/mg,'');
+    d=document.getElementsByClassName('f18b')[0].textContent.replace(/^[\s\n]|[\s\n]$/mg,'');
     i=location.href.match(/id=(\d+)/)[1];
     if(p=prompt('page?', 1)) {
         prompt('command', 'pixiv_get.pl -s -i ' + i + ' -d "' + d + '" -p ' + p);
